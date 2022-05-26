1753373b84845c997826031f0a969d9fa690eaec
MhamedElshahawy <mohamedelshahawy599@gmail.com>
2022-05-27 00:00:44 +0200

    change module path url

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,10 @@ const HtmlWebPackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 
 const deps = require("./package.json").dependencies;
-module.exports = {
+module.exports =(_,argv)=>({
   output: {
-    publicPath: "http://localhost:3002/",
+    publicPath: argv.mode === "development"?
+    "http://localhost:3002/":"https://todolist-nav.vercel.app/",
   },
 
   resolve: {
@@ -63,4 +64,4 @@ module.exports = {
       template: "./src/index.html",
     }),
   ],
-};
+});
